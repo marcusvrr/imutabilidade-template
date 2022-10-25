dd48eefccef2146b341c4c46584122fe666aef11
marcusvrr <marcusvrr2@gmail.com>
2022-10-24 22:29:40 -0300

    aula fernanda alfonsi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Garagem } from './Componentes/Garagem'
 import { GlobalStyled } from './GlobalStyled'
+import { useState } from 'react'
 
 export default function App() {
+  const[nome, setNome] = useState("Barbosa")
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome="Labenu" />
+      <Garagem nome={nome} 
+      setNome={setNome}/>
     </div>
   )
 }
--- src/Componentes/Garagem/index.js
+++ src/Componentes/Garagem/index.js
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import { Carro } from '../Carro'
 import { Botao, Estacionamento, GaragemContainer } from './styles'
 
-export function Garagem({ nome }) {
+export function Garagem({nome, setNome}) {
+  const [automovel, setAutomovel]=useState({modelo:"Corsa", cor:"Branca", ano:2020 ,flex:true})
+  const carro1 = {modelo:"Monza", cor:"Azul", ano:1999 ,flex:false};
+  const carro2 = {modelo:"Passat", cor:"Chumbo", ano:2004 ,flex:false};
+  const carro3 = {modelo:"Stilo", cor:"Verde", ano:2002 ,flex:false};
+
+  function mudaNome(nome) {
+    setNome(nome)
+  }
+
   return (
     <GaragemContainer>
       <h1>Garagem da {nome}</h1>
-      <Botao>Muda nome</Botao>
-
+      <Botao onClick={()=> mudaNome("Marcus")}>Muda nome</Botao>
+      <Botao onClick={()=> mudaCarro(carro1)}>Muda Carro</Botao>
       <Estacionamento>
         <Carro
-          modelo="Corsa"
-          cor="branco"
-          ano={2020}
-          adicionadoPor="Labenu"
-          flex
+          automovel={automovel}
+          setAutomovel={setAutomovel}
+          adicionadoPor={nome}
+          outroCarro={carro2}
+
+        />
+                <Carro
+          automovel={automovel}
+          setAutomovel={setAutomovel}
+          adicionadoPor={nome}
+          outroCarro={carro3}
+
         />
       </Estacionamento>
     </GaragemContainer>
